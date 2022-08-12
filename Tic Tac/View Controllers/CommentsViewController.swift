10b0bd5f71659220550420b9cf0a5df01f0d5f7d
Tanner Bennett <tannerbennett@me.com>
2022-08-11 23:09:24 -0500

    Support initializing CommentsVC from notification

--- Tic Tac/View Controllers/CommentsViewController.swift
+++ Tic Tac/View Controllers/CommentsViewController.swift
@@ -7,6 +7,7 @@
 
 import UIKit
 import YakKit
+import TBAlertController
 
 class CommentsViewController: FilteringTableViewController<YYComment, CommentsViewController.CommentsError> {
     enum CommentsError: LocalizedError {
@@ -27,14 +28,24 @@ class CommentsViewController: FilteringTableViewController<YYComment, CommentsVi
         }
     }
     
-    private var data: DataSourceType = .failure(CommentsError.loading) {
+    private var data: DataSourceType = .failure(.loading) {
         didSet { self.reloadData() }
     }
     
     private lazy var header: CommentsHeaderView = .init(frame: UIScreen.main.bounds)
-    private var yak: YYYak = .init()
+    private var yak: YYYak? {
+        didSet {
+            if self.isViewLoaded {
+                self.yakChanged()
+            }
+        }
+    }
     
     private var loading: Bool {
+        if self.yak == nil || self.refreshingComments {
+            return true
+        }
+        
         if case .failure(let status) = self.data, case .loading = status {
             return true
         }
@@ -42,23 +53,63 @@ class CommentsViewController: FilteringTableViewController<YYComment, CommentsVi
         return false
     }
     
+    private var refreshingComments: Bool = false {
+        didSet {
+            guard let control = self.refreshControl else { return }
+            
+            switch (self.refreshingComments, control.isRefreshing) {
+                case (true, false):
+                    control.beginRefreshing()
+                case (false, true):
+                    control.endRefreshing()
+                case (_, _):
+                    break
+            }
+        }
+    }
+    
     convenience init(for yak: YYYak) {
         self.init()
         self.yak = yak
+    }
+    
+    convenience init(from notification: YYNotification) {
+        self.init()
+        
+        YYClient.current.getYak(from: notification) { result in
+            switch result {
+                case .success(let yak):
+                    // This will update the header and reload comments
+                    self.yak = yak
+                case .failure(let error):
+                    self.data = .failure(.network(error))
+                    self.popWithError(error)
+            }
+        }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Configure table view
         self.enableRefresh = true
+        self.emptyMessage = "No Comments"
         self.tableView.separatorInset = .zero
         self.tableView.delaysContentTouches = false
         self.tableView.tableHeaderView = self.header
         
+        // Call manually to load data since the initializer won't call this
+        self.yakChanged()
+    }
+    
+    /// Update header and reload comments
+    private func yakChanged() {
+        // Configure header with yak
         self.header.configure(with: self.yak).buttonAction {
             // TODO
         }
         
+        // Reload data
         self.refresh()
     }
     
@@ -85,6 +136,20 @@ class CommentsViewController: FilteringTableViewController<YYComment, CommentsVi
             sender?.endRefreshing()
         }
     }
+    
+    /// Dismiss the comments view controller with the given error.
+    /// Used for when we couldn't load a given yak.
+    private func popWithError(_ error: Error) {
+        guard let nav = self.navigationController else {
+            return
         }
+        
+        nav.popViewController(animated: true)
+        
+        TBAlert.make({ make in
+            make.title("Error Loading Yak")
+            make.message(error.localizedDescription)
+            make.button("Dismiss")
+        }, showFrom: nav)
     }
 }
